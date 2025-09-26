218707ec6ebbbfba9902eabc88c18420150c45fd
unknown <S122322@SJ24173.ad.sbs-infosys.co.jp>
2025-09-26 15:39:08 +0900

    Create Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Header from '../components/header'
+import SpecialityMenu from '../components/SpecialityMenu'
+import TopDoctors from '../components/TopDoctors'
+import Banner from '../components/Banner'
+import Footer from '../components/Footer'
 
 const Home = () => {
     return (
         <div>
-
+            <Header></Header>
+            <SpecialityMenu />
+            <TopDoctors />
+            <Banner />
         </div>
     )
 }
