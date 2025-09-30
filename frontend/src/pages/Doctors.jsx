5ff757ff2a67c5cb9c539ff613c4f29568e7563c
unknown <S122322@SJ24173.ad.sbs-infosys.co.jp>
2025-09-30 14:19:46 +0900

    Make the website responsive

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -5,6 +5,7 @@ import { AppContext } from '../context/AppContext'
 const Doctors = () => {
     const { speciality } = useParams()
     const [filterDoc, setFilterDoc] = useState([])
+    const [showFilter, setShowFilter] = useState(false)
     const { doctors } = useContext(AppContext)
     const navigate = useNavigate();
 
@@ -25,8 +26,9 @@ const Doctors = () => {
             <p className='text-gray-600'>Browse through the doctors specialist</p>
 
             <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
+                <button className={`py-1 px-3 border border-blue-200 rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-primary text-white' : ''} `} onClick={() => setShowFilter(prev => !prev)}>Filters</button>
                 {/* ---------- Left Side -------- */}
-                <div className='flex flex-col gap-4 text-sm text-gray-600'>
+                <div className={`flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex' : 'hidden sm:flex'}`}>
                     <p onClick={() => speciality === 'General physician' ? navigate('/doctors') : navigate('/doctors/General physician')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "General physician" ? "bg-indigo-100 textblack" : ""}`}>General physician</p>
                     <p onClick={() => speciality === 'Gynecologist' ? navigate('/doctors') : navigate('/doctors/Gynecologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Gynecologist" ? "bg-indigo-100 textblack" : ""} `}>Gynecologist</p>
                     <p onClick={() => speciality === 'Dermatologist' ? navigate('/doctors') : navigate('/doctors/Dermatologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Dermatologist" ? "bg-indigo-100 textblack" : ""} `}>Dermatologist</p>
