ca59b2079ba887a1ddfd268c57189d7ac57fc380
unknown <S122322@SJ24173.ad.sbs-infosys.co.jp>
2025-10-02 17:00:53 +0900

    Create API For Admin Login

--- backend/controller/adminController.js
+++ backend/controller/adminController.js
@@ -1,7 +1,8 @@
 import validator from 'validator'
 import bcrypt from 'bcrypt'
 import { v2 as cloudinary } from 'cloudinary'
-import doctorModel from '../models/doctoreModel'
+import doctorModel from '../models/doctorModel'
+import jwt from 'jsonwebtoken'
 
 //API for adding doctor
 const addDoctor = async (req, res) => {
@@ -56,4 +57,22 @@ const addDoctor = async (req, res) => {
     }
 }
 
-export { addDoctor }
+//API for Admin Login
+const loginAdmin = async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        if (email === process.env.ADMIN_EMAIl && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email + password, process.env.JWT_SECRET)
+            res.json({ success: true, token })
+        } else {
+            res.json({ success: false, message: "Invalid credentials" })
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+export { addDoctor, loginAdmin }
