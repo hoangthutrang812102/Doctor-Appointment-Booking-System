b68371e5648ff5411d1521fe8c41c7633d5cece9
unknown <S122322@SJ24173.ad.sbs-infosys.co.jp>
2025-10-06 14:34:06 +0900

    Display all doctors list in Admin panel

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,11 +1,15 @@
 import express from 'express'
-import { addDoctor, loginAdmin } from '../controller/adminController.js'
+import { addDoctor, loginAdmin, allDoctors } from '../controller/adminController.js'
 import upload from '../middlewares/multer.js'
 import authAdmin from '../middlewares/authAdmin.js'
+import { changeAvailability } from '../controller/doctorController.js'
 
 const adminRouter = express.Router()
 
 adminRouter.post('/add-doctor', authAdmin, upload.single('image'), addDoctor)
 adminRouter.post('/login', loginAdmin)
+adminRouter.post('/all-doctors', authAdmin, allDoctors)
+adminRouter.post('/change-availability', authAdmin, changeAvailability)
+
 
 export default adminRouter
